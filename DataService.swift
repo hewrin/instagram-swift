7b00ba970dcbeba4ad6da03e96851cbba2d4ce75
Faris <hewrin@hotmail.com>
2016-04-22 10:51:09 +0800

    create required pods

---
+++ DataService.swift
@@ -0,0 +1,14 @@
+//
+//  DataService.swift
+//  InstagramClone
+//
+//  Created by Faris Roslan on 22/04/2016.
+//  Copyright © 2016 Faris Roslan. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+class DataService {
+  static let dataService = DataService()
+}
---
+++ Pods/Target Support Files/Pods-InstagramClone/Pods-InstagramClone-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_InstagramCloneVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_InstagramCloneVersionString[];
+
