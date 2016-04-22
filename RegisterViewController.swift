5eb49fab888d5a5f1df8d827906671f35744ef8d
Joanne Lim <joannelim@Joannes-MacBook-Air.local>
2016-04-22 15:47:58 +0800

    created basic storyboard and user login signup

---
+++ RegisterViewController.swift
@@ -0,0 +1,54 @@
+//
+//  RegisterViewController.swift
+//  InstagramClone
+//
+//  Created by Joanne Lim on 22/4/16.
+//  Copyright © 2016 Faris Roslan. All rights reserved.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor.cyanColor()
+
+        
+    }
+
+    @IBAction func submitButtonPressed(sender: AnyObject) {
+        if let username = usernameTextField.text, email = emailTextField.text, password = passwordTextField.text{
+            let rootReference = DataService.dataService.BASE_REF
+            rootReference.createUser(email, password: password, withValueCompletionBlock: { (error, result) in
+                if error == nil{
+                    let uid = result ["uid"] as? String
+                    
+                    let userDict = ["email":email, "username":username]
+                    
+                    let currentUser = rootReference.childByAppendingPath("users").childByAppendingPath(uid)
+                    
+                    currentUser.setValue(userDict)
+                    
+                    NSUserDefaults.standardUserDefaults().setValue(uid, forKey: "uid")
+                    self.performSegueWithIdentifier("Loggedin", sender: nil)
+                    // can consider for the login page to jump back to the login page
+                    
+                }else{
+                    
+                    let alertController = UIAlertController(title: "Error Message", message: "\(error.localizedDescription)", preferredStyle: .Alert)
+                    
+                    let defaultAction = UIAlertAction(title: "OK", style: .Default, handler: nil)
+                    alertController.addAction(defaultAction)
+                    
+                    self.presentViewController(alertController, animated: true, completion: nil)
+                }
+            })
+            
+        }
+    }
+
+}
---
+++ SearchTableViewController.swift
@@ -0,0 +1,95 @@
+//
+//  SearchTableViewController.swift
+//  InstagramClone
+//
+//  Created by Joanne Lim on 22/4/16.
+//  Copyright © 2016 Faris Roslan. All rights reserved.
+//
+
+import UIKit
+
+class SearchTableViewController: UITableViewController {
+    @IBOutlet weak var searchTextField: UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+        
+    }
+
+    
+    @IBAction func searchButtonPressed(sender: AnyObject) {
+        
+        
+        
+    }
+
+//    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+//   
+//        
+//        return 0
+//    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+   
+        
+        return 0
+    }
+
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath)
+
+ 
+        
+
+        return cell
+    }
+
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == .Delete {
+            // Delete the row from the data source
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+        } else if editingStyle == .Insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
