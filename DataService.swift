520d9ef2de4bb808af7e0fb9def5fb0c43de1536
Joanne Lim <joannelim@Joannes-MacBook-Air.local>
2016-04-26 11:29:12 +0800

    added a new viewcontroller

--- DataService.swift
+++ DataService.swift
@@ -12,9 +12,22 @@ import Firebase
 class DataService {
     static let dataService = DataService()
     private let _BASE_URL = Firebase(url: BASE_URL)
+    private let _USER_REF = Firebase(url: "\(BASE_URL)/users")
     
     var BASE_REF: Firebase{
         return _BASE_URL
+    }
+    
+    var USER_REF: Firebase{
+        return _USER_REF
+    }
+    
+    var CURRENT_USER_REF: Firebase{
+        var currentRef: Firebase?
+        if let currentUserID = NSUserDefaults.standardUserDefaults().valueForKey("uid") as? String{
+            currentRef = USER_REF.childByAppendingPath(currentUserID)
+        }
+        return currentRef!
     }
     
 }
