869d63fbe2b6301ec7d57911dbf359eca81eacb6
Joanne Lim <joannelim@Joannes-MacBook-Air.local>
2016-04-27 14:37:49 +0800

    searchview updated with username

--- SearchTableViewController.swift
+++ SearchTableViewController.swift
@@ -10,24 +10,21 @@ import UIKit
 
 class SearchTableViewController: UITableViewController {
     @IBOutlet weak var searchTextField: UITextField!
-    var usernameID = String()
+    var usernameID = [user]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        DataService.dataService.USER_REF.observeEventType(.ChildAdded, withBlock:  { (snapshot) in
+            if let value = snapshot.value as? [String : AnyObject]{
+                let userName = user(key: snapshot.key, dict: value)
+                
+                self.usernameID.append(userName)
+                
+                self.tableView.reloadData()
+            }
+        })
+
         
-//        DataService.dataService.USER_REF.observeEventType(<#T##eventType: FEventType##FEventType#>, withBlock: <#T##((FDataSnapshot!) -> Void)!##((FDataSnapshot!) -> Void)!##(FDataSnapshot!) -> Void#>)
-//            if let value = snapshot.value as? [String : AnyObject] {
-//              let userID = usernameID(key: snapshot.key , dict: value)
-//        }
-//        
-//        
-//        
-//        DataService.dataService.TWEET_REF.observeEventType(.ChildAdded, withBlock: { (snapshot) -> Void in
-//            //returns value of snapshot , and assign values to tweet
-//            if let value = snapshot.value as? [String : AnyObject] {
-//                let tweet = Tweet(key: snapshot.key , dict: value)
-//                self.tweets.append(tweet)
-//                self.tableView.reloadData()
     }
 
     
@@ -37,71 +34,22 @@ class SearchTableViewController: UITableViewController {
         
     }
 
-//    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-//   
-//        
-//        return 0
-//    }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
    
         
-        return 0
+        return usernameID.count
     }
 
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath)
 
- 
+        let userName = self.usernameID[indexPath.row]
+        cell.textLabel?.text = userName.username
         
 
         return cell
     }
 
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
